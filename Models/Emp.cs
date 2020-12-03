47c7e2b0b3c7992473f9b930d0a355b8b102a3d2
AyushKumar <ayushaktu1234@gmail.com>
2020-12-03 15:52:57 +0530

    Add model EMP

---
+++ Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppExercise2nd_GitHub_Branches_03_12.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public double ESalary { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppExercise2nd_GitHub_Branches_03_12.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
